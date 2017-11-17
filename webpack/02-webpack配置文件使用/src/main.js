b74109bc50046dcee704b42f83fad280c9381c87
bifjhh <929160004@qq.com>
2017-11-17 14:20:48 +0800

    webpack配置文件使用

---
+++ webpack/02-webpack配置文件使用/src/main.js
@@ -0,0 +1,16 @@
+// 1 获取dom对象
+var v1 = document.querySelector('#v1');
+var v2 = document.querySelector('#v2');
+var bt = document.querySelector('#bt');
+var res = document.querySelector('#res');
+
+bt.onclick = function () {
+
+    // 2 获取calc.js中的add方法并且调用计算结果
+    var v1value = parseFloat(v1.value);
+    var v2value = parseFloat(v2.value);
+
+    // 调用add方法
+    var add = require('./calc.js');
+    res.value = add(v1value, v2value);
+}
